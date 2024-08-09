ce31e283f9c47474256c47dcbdb4b0686fbb9496
Atomfusion <md15uakron@gmail.com>
2024-08-08 21:31:19 -0400

    Update PrivateCode.cs

--- E3Next/Processors/PrivateCode.cs
+++ E3Next/Processors/PrivateCode.cs
@@ -61,7 +61,7 @@ namespace E3Core.Processors
         public static void Init()
         {
             Casting.VarsetValues.Add("TimeTillDeath", "10"); // Add TimeTillDeath and use in Ifs
-            MQ.Write("\agPrivate Code Version 1.2.4 Loaded ");
+            MQ.Write("\agPrivate Code Version 1.2.5 Loaded ");
             MQ.Cmd("/nav setopt dist=30 log=off");
             // Initialize Variables One Time
             CampOutTimer = new ElapsedTimer();
